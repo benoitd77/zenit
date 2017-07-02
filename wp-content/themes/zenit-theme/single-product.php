93de0f339ccb14600787bd58e84cc485f6662c26
Benoit <benoitd77@gmail.com>
2017-07-02 18:31:15 -0400

    moved css from template to stylesheet

--- wp-content/themes/zenit-theme/single-product.php
+++ wp-content/themes/zenit-theme/single-product.php
@@ -1,122 +1,32 @@
 <script language="javascript">
 
-function adjustColHeight() {
-	var colRight = document.getElementById('single-prod');
-	var colMenu  = document.getElementById('fixed-menu-single-product');
-	var colRightHeight = 0;
+	function adjustColHeight() {
+		var colRight = document.getElementById('single-prod');
+		var colMenu  = document.getElementById('fixed-menu-single-product');
+		var colRightHeight = 0;
 
-	if (colRight != null && colMenu != null) {
-		colMenu.style.height = colRight.offsetHeight + "px";
+		if (colRight != null && colMenu != null) {
+			colMenu.style.height = colRight.offsetHeight + "px";
+		}
 	}
-}
 
-for (var i = 500; i <= 3500; i+=1000) {
-	setTimeout(function(){
-		adjustColHeight();
-	}, i);
-}
+	for (var i = 500; i <= 3500; i+=1000) {
+		setTimeout(function(){
+			adjustColHeight();
+		}, i);
+	}
 
-if(window.addEventListener) {
-    window.addEventListener('resize', function() {
-        adjustColHeight();
-    }, true);
+	if(window.addEventListener) {
+	    window.addEventListener('resize', function() {
+	        adjustColHeight();
+	    }, true);
 
-    window.addEventListener("load", function() {
-        console.log("All resources finished loading!");
-    });
-}
+	    window.addEventListener("load", function() {
+	        console.log("All resources finished loading!");
+	    });
+	}
 	
 </script>
-<style>
-	body.single-product .sale-section .board {
-		margin-left: 5% !important;
-		margin-right: 5% !important;
-		width: 90%;
-	}
-
-	body.single-product .image-board {
-        width: 100%;
-    }
-
-	body.single-product .image-board img {
-		max-height: initial;
-		width: 100%;
-	}
-
-	#fixed-menu-single-product {
-		position: initial !important;
-		height: 1000px;
-		width: 190px;
-		float: left;
-		box-shadow: none;
-		border-right: 2px solid #e6e6e6;
-	}
-
-	#fixed-menu-single-product ul {
-		position: fixed;
-	}
-
-	div.single-product.col2-layout {
-		width: calc(100% - 190px);
-	    float: right;
-	}
-
-	#bottom-sales-section {
-		background-color: #e6e6e6 !important;
-		padding: 30px !important;
-		position: relative;
-	}
-
-	#bottom-sales-section .outer-anchor {
-		position: absolute;
-		top: -80px;
-	}
-
-	#bottom-sales-section .row > div {
-		background-color: #ffffff;
-		padding: 40px 0 50px;
-	}
-
-	@media (min-width: 768px) {
-		#bottom-sales-section .row > div {
-		    width: 49%;
-		    margin-left: 2%;
-		    padding: 2.5% 0 2.5%;
-		}
-
-		#bottom-sales-section .row > div:first-child {
-			margin-left: 0;
-		}
-	}
-
-	@media (max-width: 925px) {
-		body.single-product .sale-section p {
-			font-size: 15px;
-		}
-	}
-
-	@media (max-width: 810px) {
-		body.single-product .sale-section p {
-			font-size: 14px;
-		}
-	}
-
-	@media (max-width: 767px) {
-		body.single-product .sale-section p {
-			font-size: 18px;
-		}
-		
-		div.single-product,
-		div.single-product.col2-layout {
-			width: 100%;
-			float: none;
-		}
-
-		#bottom-sales-section .row > div:first-child {
-			margin-bottom: 20px;
-		}
-	}
-</style>
 
 <?php
 
@@ -127,7 +37,7 @@ if (have_rows('section')) {
     $col2Layout = true;
     ?>
 	<div id="fixed-menu-single-product">
-		<ul>
+		<ul class="menu-single-product">
 			<li><a href="#stats"><?php _e('Stats','zenit'); ?></a></li>
 		    <?php while ( have_rows('section') ) : the_row();
 		       
