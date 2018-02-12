11817b019ba10f6d91dc763cd637afa142e78236
Benoit <benoitd77@gmail.com>
2018-02-11 21:47:46 -0500

    Corrections diverses

--- wp-content/themes/zenit-theme/templates/page-vetements.php
+++ wp-content/themes/zenit-theme/templates/page-vetements.php
@@ -5,22 +5,6 @@
 ?>
 
 <section id="product-list" class="container-fluid">
-
-
-	<?php
-		$currentLang = qtrans_getLanguage();
-
-		if ($currentLang === 'fr'): ?>
-			<h1 style="text-align: center;">Cette page sera bientôt disponible</h1>
-		<?php else: ?>
-			<h1 style="text-align: center;">This page is coming soon</h1>
-		<?php endif;
-	?>
-
-
-
-
-
 	<?php
 	wc_print_notices();
 
@@ -61,7 +45,7 @@
 				<?php while ( $loop->have_posts() ) : $loop->the_post();
 					$_product = wc_get_product( get_the_ID() ); ?>
 
-					<div class="col-sm-4 col-xs-6 default-product">
+					<div class="col-sm-3 col-xs-6 default-product">
 						<div class="content-default">
 							<div class="section-top">
 								<h3><?php echo get_the_title(); ?></h3>
